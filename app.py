b5256df9b184e9078831c36ac8618bcc55b87931
SOMA-NITHIN <somanithin123@gmail.com>
2026-04-29 17:04:19 +0800

    Add Flask routes and convert home/login templates

--- app.py
+++ app.py
@@ -2,20 +2,46 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# homepage
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-# 如果你有其他页面，比如 login / about，可以这样加
 @app.route("/login")
 def login():
     return render_template("login.html")
 
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 
-# 启动服务器
+@app.route("/ingredients")
+def ingredient_selection():
+    return render_template("ingredient-selection.html")
+
+
+@app.route("/community")
+def community():
+    return render_template("community.html")
+
+
+@app.route("/saved")
+def saved_recipes():
+    return render_template("SavedRecipe.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/recipe")
+def recipe_detail():
+    return render_template("recipe-detail.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
