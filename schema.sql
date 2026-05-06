75281c9a57642b2872dbf5cd8ed566e7afce3ffa
AmyYang <25093361@student.uwa.edu.au>
2026-05-06 14:32:12 +0800

    changed the hardcode part down the end of the page: likes/users/ingredients.linked it to db.

--- schema.sql
+++ schema.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE ingredients (
+CREATE TABLE IF NOT EXISTS ingredients (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
 );
-CREATE TABLE recipes (
+CREATE TABLE IF NOT EXISTS recipes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     description TEXT,
@@ -18,14 +18,14 @@ CREATE TABLE recipes (
     user_id INTEGER,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
-CREATE TABLE recipe_ingredients (
+CREATE TABLE IF NOT EXISTS recipe_ingredients (
     recipe_id INTEGER,
     ingredient_id INTEGER,
     quantity TEXT,
     FOREIGN KEY (recipe_id) REFERENCES recipes(id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
 );
-CREATE TABLE saved_recipes (
+CREATE TABLE IF NOT EXISTS saved_recipes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     recipe_id INTEGER NOT NULL
