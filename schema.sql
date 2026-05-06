89c1f542b6f4ed56b5cbf14d81e6acc25791614d
AmyYang <25093361@student.uwa.edu.au>
2026-05-06 17:15:02 +0800

    merged schema.sql

--- schema.sql
+++ schema.sql
@@ -13,3 +13,23 @@ CREATE TABLE IF NOT EXISTS saved_recipes (
     PRIMARY KEY (user_id, recipe_id),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS ingredients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS recipes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    instructions TEXT,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+CREATE TABLE IF NOT EXISTS recipe_ingredients (
+    recipe_id INTEGER,
+    ingredient_id INTEGER,
+    quantity TEXT,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
+);
