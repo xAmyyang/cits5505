aea55d877ba10769e5a5334a0f2fc31f41a2be74
AmyYang <25093361@student.uwa.edu.au>
2026-05-06 14:11:21 +0800

    added 1.ingredients 2.recipes 3.recipe_ingredients 4.saved_recipes schemas.

--- schema.sql
+++ schema.sql
@@ -5,3 +5,28 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE ingredients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+CREATE TABLE recipes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    instructions TEXT,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+CREATE TABLE recipe_ingredients (
+    recipe_id INTEGER,
+    ingredient_id INTEGER,
+    quantity TEXT,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
+);
+CREATE TABLE saved_recipes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    recipe_id INTEGER NOT NULL
+);
